e644c9cea3d2d36480b153cbe0d063ba469ae309
jf <fajardo_jf@hotmail.com>
2013-03-16 12:10:53 +1100

    [ModernWeb/WebUI] Rectangle Control

---
+++ ModernWeb/Framework/Controls/WebUI/controls.rectangle.ts
@@ -0,0 +1,54 @@
+﻿/// <reference path="controls.controlbase.ts"/>
+/// <reference path="experience.ts"/>
+
+declare var $;
+
+class Rectangle extends ControlBase
+{
+
+    public Slot: number;
+    public Color : string;
+    public StoryboardOnLoad: number;
+
+    public zIndex: number;
+
+
+    constructor(experience: Experience, slot: number, bkgcolor: string, sbonload: any){
+        super(experience);
+
+        this.Slot = slot;
+        this.Color = bkgcolor;
+        this.StoryboardOnLoad = sbonload;
+    }
+
+    public Initialize()
+    {
+        this.zIndex += 3;
+        //bg.Print("Rectangle Initialize " + slot);
+    }
+
+    public Update(tick)
+    {
+    }
+
+    public Draw(surface)
+    {
+        if (this.IsVisible)
+        {
+
+            //Dbg.Print("this.Storyboard.AnimArea : " + this.Storyboard.AnimArea);
+            surface.globalAlpha = this.Opacity();
+            surface.fillStyle = this.Color;
+            surface.fillRect(this.X(), this.Y(), this.Width(), this.Height());
+            //Dbg.Print("this.Opacity() : " + this.Opacity());
+            
+        }
+    }
+
+    public Unload()
+    {
+
+    }
+
+
+}
