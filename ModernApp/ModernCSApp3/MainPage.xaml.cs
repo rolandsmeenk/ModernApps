ba4084ffe1979807873cc6da78b2eb2ab38881b3
Jose Fajardo <fajardo_jf@hotmail.com>
2013-12-13 22:47:15 +1100

    Dot tile
    added dot related animations to the tile

--- ModernApp/ModernCSApp3/MainPage.xaml.cs
+++ ModernApp/ModernCSApp3/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,29 @@ namespace ModernCSApp3
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void butStart_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            Random rnd = new Random();
+            int startMilliseconds = rnd.Next(10, 600);
+            ctlFukingAmazingTile1.StartAnimation(new SolidColorBrush(Colors.Green), startMilliseconds, ModernTile.eAnimationType.CenterDot);
+            startMilliseconds = rnd.Next(10, 200);
+            ctlFukingAmazingTile2.StartAnimation(new SolidColorBrush(Colors.Red), startMilliseconds, ModernTile.eAnimationType.CenterDot);
+            startMilliseconds = rnd.Next(10, 200);
+            ctlFukingAmazingTile3.StartAnimation(new SolidColorBrush(Colors.Pink), startMilliseconds, ModernTile.eAnimationType.CenterDot);
+            startMilliseconds = rnd.Next(10, 200);
+            ctlFukingAmazingTile4.StartAnimation(new SolidColorBrush(Colors.Yellow), startMilliseconds, ModernTile.eAnimationType.CenterDot);
+            startMilliseconds = rnd.Next(10, 200);
+            ctlFukingAmazingTile5.StartAnimation(new SolidColorBrush(Colors.Orange), startMilliseconds, ModernTile.eAnimationType.CenterDot);
+            startMilliseconds = rnd.Next(10, 200);
+            ctlFukingAmazingTile6.StartAnimation(new SolidColorBrush(Colors.Purple), startMilliseconds, ModernTile.eAnimationType.CenterDot);
+            startMilliseconds = rnd.Next(10, 200);
+            ctlFukingAmazingTile7.StartAnimation(new SolidColorBrush(Colors.Brown), startMilliseconds, ModernTile.eAnimationType.CenterDot);
+            startMilliseconds = rnd.Next(10, 200);
+            ctlFukingAmazingTile8.StartAnimation(new SolidColorBrush(Colors.Blue), startMilliseconds, ModernTile.eAnimationType.CenterDot);
+            startMilliseconds = rnd.Next(10, 200);
+            ctlFukingAmazingTile9.StartAnimation(new SolidColorBrush(Colors.LightCyan), startMilliseconds, ModernTile.eAnimationType.CenterDot);
         }
     }
 }
--- ModernApp/ModernCSApp3/ModernTile.xaml.cs
+++ ModernApp/ModernCSApp3/ModernTile.xaml.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
@@ -19,9 +20,64 @@ namespace ModernCSApp3
 {
     public sealed partial class ModernTile : UserControl
     {
+        bool isDotShowing = false;
+        bool hasInitialized = false;
+
+        public enum eAnimationType
+        {
+            CenterDot = 0
+        }
+
         public ModernTile()
         {
             this.InitializeComponent();
+            
+        }
+
+
+        public void StartAnimation(SolidColorBrush fill, int startMilliseconds, eAnimationType animationType)
+        {
+
+            if(!hasInitialized){
+                layoutRoot.Clip.Rect = new Rect(0, 0, this.ActualWidth, this.ActualHeight);
+                
+                lblLine1.Text = fill.Color.ToString();
+
+                if (animationType == eAnimationType.CenterDot) { 
+                    ctlGrowingDot.Fill = fill;
+
+                    double widthToUse = this.ActualWidth > this.ActualHeight ? this.ActualWidth : this.ActualHeight;
+
+                    //growdot
+                    ((DoubleAnimationUsingKeyFrames)sbGrowDot.Children[0]).KeyFrames[0].KeyTime = TimeSpan.FromMilliseconds(startMilliseconds);
+                    ((DoubleAnimationUsingKeyFrames)sbGrowDot.Children[0]).KeyFrames[1].Value = widthToUse * 2;
+
+                    ((DoubleAnimationUsingKeyFrames)sbGrowDot.Children[1]).KeyFrames[0].KeyTime = TimeSpan.FromMilliseconds(startMilliseconds);
+                    ((DoubleAnimationUsingKeyFrames)sbGrowDot.Children[1]).KeyFrames[1].Value = widthToUse * 2;
+
+                    //shrinkdot
+                    ((DoubleAnimationUsingKeyFrames)sbShrinkDot.Children[0]).KeyFrames[0].KeyTime = TimeSpan.FromMilliseconds(400 + startMilliseconds);
+                    ((DoubleAnimationUsingKeyFrames)sbShrinkDot.Children[1]).KeyFrames[0].KeyTime = TimeSpan.FromMilliseconds(400 + startMilliseconds);
+
+                    //animatetext
+                    ((DoubleAnimationUsingKeyFrames)sbAnimateText.Children[0]).KeyFrames[0].KeyTime = TimeSpan.FromMilliseconds(startMilliseconds + 300);
+                    ((DoubleAnimationUsingKeyFrames)sbAnimateText.Children[0]).KeyFrames[1].KeyTime = TimeSpan.FromMilliseconds(startMilliseconds + 300 + 1500);
+
+                }
+
+                hasInitialized = true;
+            }
+
+
+            if (animationType == eAnimationType.CenterDot) {
+                if (isDotShowing) { sbShrinkDot.Begin(); sbAnimateText.Stop(); }
+                else { sbGrowDot.Begin(); sbAnimateText.Begin(); }
+
+                isDotShowing = !isDotShowing;
+            }
+
+
+
         }
     }
 }
